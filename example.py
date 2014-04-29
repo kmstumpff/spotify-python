9b727a5c3a325af7c91f314752f1536921f6ec81
kyle <kmstumpff@gmail.com>
2014-04-28 22:36:30 -0400

    Adding files

---
+++ example.py
@@ -0,0 +1,152 @@
+import spotify, os, sys
+
+global sQuery
+sQuery = spotify.Query()
+
+global menu
+menu = "Song Search Menu:\n1. Open a link to a song\n2. Search again\n3. Quit\n\nEnter choice: "
+
+
+#####################################
+# Function:     disp_meta
+#
+# Description:  displays metadata information about the query
+#
+# Parameters:   None
+#
+# Returns:      None
+#
+#####################################
+def disp_meta():
+    if sQuery.num_results > 0:
+        print("Number of results:" + str(sQuery.num_results))
+    else:
+        print("No data found")
+
+
+#####################################
+# Function:     open_link
+#
+# Description:  Prompts user for result number then opens the url of the song the user selects.
+#
+# Parameters:   None
+#
+# Returns:      None
+#
+#####################################
+def open_link():
+    choice = int(raw_input("Please enter the number you want to open: "))
+    if check_range(choice, 1, sQuery.length):
+        #We need to open the url differently between OS's
+        if sys.platform[:6] == "darwin":
+            os.system("open " + sQuery.get_url(choice - 1))
+        elif sys.platform[:3] == "win":
+            os.system("start " + sQuery.get_url(choice - 1))
+        elif sys.platform[:5] == "linux":
+            os.system("firefox " + sQuery.get_url(choice - 1))
+        else:
+            print("Unsupported platform: Cannot open URL...")
+    else:
+        print("Invalid answer: Try again!")
+        open_link()
+    print("\n")
+    disp_menu()
+
+
+def search_again():
+    program()
+
+
+def sys_exit():
+    sys.exit(0)
+
+# This is used for a C++ like case/switch statement
+global menu_options
+menu_options = {1: open_link,
+                2: search_again,
+                3: sys_exit,
+}
+
+
+#####################################
+# Function:     check_range
+#
+# Description:  checks if the number is between two values
+#
+# Parameters:   number - the number being compared
+#               lower - the lower bound (is included)
+#               upper - the upper bound (is included)
+#
+# Returns:      True if the number is within the range or False if it is outside the bounds
+#
+#####################################
+def check_range(number, lower, upper):
+    if upper >= lower <= number:
+        return True
+    else:
+        return False
+
+
+#####################################
+# Function:     disp_menu
+#
+# Description:  Displays a menu after the query asking user to 1. open link 2. search again 3. exit.
+#
+# Parameters:   None
+#
+# Returns:      None
+#
+#####################################
+def disp_menu():
+    ans_menu = int(raw_input(menu))
+    if check_range(ans_menu, 1, 3):
+        # This is used for a C++ like case/switch statement
+        menu_options[ans_menu]()
+    else:
+        print("Invalid option. Try again!")
+        disp_menu()
+
+
+#####################################
+# Function:     program
+#
+# Description:  This is the main function
+#
+# Parameters:   None
+#
+# Returns:      None
+#
+#####################################
+def program():
+    print("\nDo you want to search for Album(1), Artist(2), or Track(3)?\n")
+    s_type = raw_input("Enter number: ")
+    if check_range(s_type, 1, 3):
+        query = raw_input("Enter a search query: ")
+        sQuery.api_call(query, int(s_type))
+        disp_meta()
+        if not sQuery.json:
+            print("No match found!")
+        else:
+            while True:
+                limit = int(raw_input(str(sQuery.length) + " matches found. How many would you like to display? "))
+                if check_range(limit, 1, sQuery.length):
+                    break
+                else:
+                    print("You entered an invalid number. Try again!")
+            for x in range(0, limit):
+                print("  Result: " + str(x + 1))
+                print("-------------------------------------------")
+                print("| Song: " + sQuery.get_title(x))
+                print("| Artist: " + sQuery.get_artist(x))
+                print("| Album: " + sQuery.get_album(x))
+                print("| Url: " + sQuery.get_url(x))
+                print("-------------------------------------------\n")
+            disp_menu()
+    else:
+        print("Invalid option. Try again!")
+        program()
+
+
+# Run the main program
+if __name__ == "__main__":
+    program()
---
+++ spotify.py
@@ -0,0 +1,81 @@
+import urllib2
+import json
+
+
+class Query:
+    def __init__(self):
+        self.metadata = []
+        self.json = []
+        self.length = 0
+        self.num_results = 0
+        self.api_key = ''  # fill in with your key
+        self.api_url = 'http://ws.spotify.com/search/1/'
+        self.media_type = 0
+        self.api_media = 'album'
+        self.ret_choice = ""
+
+    def check_results(self):
+        if self.json:
+            return True
+        else:
+            return False
+
+    def get_title(self, numb):
+        if self.media_type == 3:
+            return self.json[numb]['name']
+        else:
+            return "No track available when searching album or artist"
+
+    def get_artist(self, numb):
+        if self.media_type == 2:
+            return self.json[numb]['name']
+        else:
+            temp = self.json[numb]['artists']
+            return temp[0]['name']
+
+    def get_album(self, numb):
+        if self.media_type == 1:
+            return self.json[numb]['name']
+        else:
+            temp = self.json[numb]['album']
+            return temp['name']
+
+    def get_url(self, numb):
+        return self.json[numb]['href']
+
+    def api_call(self, query, s_type):
+        print("query: " + query)
+        print("s_type: " + str(s_type))
+        self.media_type = s_type
+        if s_type == 1:
+            self.api_media = "albums"
+            req = self.api_url + "album.json?q=" + query.replace(" ", "+")
+            response = json.loads(urllib2.urlopen(req.lower()).read())
+            self.metadata = response['info']
+            self.json = response['albums']
+        if s_type == 2:
+            self.api_media = "artists"
+            req = self.api_url + "artist.json?q=" + query.replace(" ", "+")
+            response = json.loads(urllib2.urlopen(req.lower()).read())
+            self.metadata = response['info']
+            self.json = response['artists']
+        if s_type == 3:
+            self.api_media = "tracks"
+            req = self.api_url + "track.json?q=" + query.replace(" ", "+")
+            response = json.loads(urllib2.urlopen(req.lower()).read())
+            self.metadata = response['info']
+            self.json = response['tracks']
+        else:
+            print("Error: API call failed...")
+        print("request: " + req)
+        self.length = len(self.json)
+        self.num_results = self.metadata['num_results']
+
+
+class APIError(Exception):
+    def __init__(self, value):
+        self.message = value
+
+    def __str__(self):
+        return repr("There was a problem with your API request: " + self.message)
+        
